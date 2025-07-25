57207d9b86701e9900749d63012e27072e8c14f1
Saismruti Das <104313895+saismruti-das@users.noreply.github.com>
2025-07-25 23:44:29 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+//fibonnacci series:
+import java.util.Scanner;
+public class fibonnacci{
+    static int n1=0,n2=1,n3;
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("enter number :");
+            int n=sc.nextInt();
+            System.out.print("0 1 ");
+            printFib(n);
+        }
+    }
+    
+    static void printFib(int n){
+        if(n==0){
+            return;
+        }
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+        System.out.print(n3+" ");
+        printFib(n-1);
+    }
+}
