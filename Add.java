d8638abb1f587d2c9298afb3a6af81afc52660d2
saismruti-das <das.sai1312@gmail.com>
2025-08-17 23:35:05 +0530

    Add adding two number

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Add {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a:");
+        a=sc.nextInt();
+        System.out.println("Enter b:");
+        b=sc.nextInt();
+        System.out.println("Sum="+(a+b));
+    }
+}
