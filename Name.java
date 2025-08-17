9d946a57d43be1acaded375b917245d6bd6ec5b8
saismruti-das <das.sai1312@gmail.com>
2025-08-17 23:32:59 +0530

    Add simple program

---
+++ Name.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Name {
+    public static void main(String[] args){
+        String n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Name: ");
+        n=sc.nextLine();
+        System.out.println("My name is "+n);
+    }
+}
