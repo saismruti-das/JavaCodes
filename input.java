96872dcb3cffddfeb47baea4d0980b0d93f00f93
saismruti-das <das.sai1312@gmail.com>
2025-08-23 01:38:18 +0530

    Take two input by user and find sum

--- input.java
+++ input.java
@@ -2,7 +2,10 @@ import java.util.*;
 public class input {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String name=sc.next();
+        String name=sc.next();                      //can only take one token. i.e. if we enter "tony stack" it will only take tony as input.
+        //nextLine() --> for taking whole line
+        //nextInt() --> for integer type input
+        //nextFloat() 
         System.out.println(name);
     }
 }
