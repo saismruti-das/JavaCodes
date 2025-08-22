df772cf797e50c1218614f2cabc0fff32aabc0ca
saismruti-das <das.sai1312@gmail.com>
2025-08-23 01:24:05 +0530

    Taking input in java

--- variables.java
+++ variables.java
@@ -2,7 +2,6 @@ public class variables {
     public static void main(String[] args) {
         int a=10;
         int b=25;
-        int sum=a+b;
         int mul=a*b;
         System.out.println(mul);
     }
