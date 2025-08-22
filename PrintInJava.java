580bad46f95fd37707ecebdc0937114cda9f7f4c
saismruti-das <das.sai1312@gmail.com>
2025-08-23 00:51:33 +0530

    Learn about print statement

---
+++ PrintInJava.java
@@ -0,0 +1,8 @@
+public class PrintInJava {
+    public static void main(String[] args){
+        System.out.print("Hello World \n"); //escape sequence does same work as below
+        System.out.println("Hello World");  //moves cursor to next line after output
+        System.out.print("Hello World");
+        System.out.print("Hello \n World \n Hello World");
+    }
+}
