90c4a8d2fca6398968d9b193b86f94a1559b1327
saismruti-das <das.sai1312@gmail.com>
2025-08-23 00:52:16 +0530

    simple pattern prit

--- PrintInJava.java
+++ PrintInJava.java
@@ -1,8 +1,16 @@
 public class PrintInJava {
     public static void main(String[] args){
-        System.out.print("Hello World \n"); //escape sequence does same work as below
-        System.out.println("Hello World");  //moves cursor to next line after output
-        System.out.print("Hello World");
-        System.out.print("Hello \n World \n Hello World");
+        //pattern 
+        /*
+        *
+        **
+        ***
+        ****
+         */
+        
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
     }
 }
